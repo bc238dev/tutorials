67577c6122128c7b703b2e9d2b455a43aa617c1a
Ben Dev <bc238dev@gmail.com>
2019-08-11 19:27:14 -0700

    Fixing DataFetcher error.

--- book-details/src/main/java/com/graphqljava/tutorial/bookdetails/BookDetailsApplication.java
+++ book-details/src/main/java/com/graphqljava/tutorial/bookdetails/BookDetailsApplication.java
@@ -1,5 +1,7 @@
 package com.graphqljava.tutorial.bookdetails;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookDetailsApplication {
 
     public static void main(String[] args) {
+        System.out.println("GraphQL demo starting on " + new Date());
         SpringApplication.run(BookDetailsApplication.class, args);
     }
-
 }
 
